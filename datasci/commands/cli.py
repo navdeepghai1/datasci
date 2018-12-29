461c7b0467796ebf3dfa302aa84cf0deb7138926
navdeepghai1 <navdeepghai1@gmail.com>
2018-12-29 10:36:26 +0400

    Validation to ensure root user don't execute the command and command should be execute from env and datasci context

--- datasci/commands/cli.py
+++ datasci/commands/cli.py
@@ -1,12 +1,26 @@
+'''
+	Developer Navdeep
+	Email navdeepghai1@gmail.com
+'''
 
 import click
 import os
 import sys
 
+'''
+	Entry point for Commands
+'''
 def execute_from_command_line(*args, **kwargs):
 
 	from .commands import commands
+	from.utils import validate_execution_path, is_root_user
 	temp = dict()
+	if is_root_user():
+		click.secho("You can't execute the command from root user", fg="red")
+		return 
+	if not validate_execution_path():
+		click.secho("Execute the command from env context", fg="yellow")
+		return 
 	for command in commands:
 		temp.update({command.name: command})
 	group  = dict(datasci=click.group(name="datasci", commands=temp)(file_path))
