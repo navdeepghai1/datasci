233e3dfbdb08c0fe6c163f32bad18c0346cf63ca
navdeepghai1 <navdeepghai1@gmail.com>
2018-12-29 12:08:01 +0400

    Working on Commands line tool

--- datasci/commands/commands.py
+++ datasci/commands/commands.py
@@ -12,10 +12,21 @@ import subprocess
 @click.option("--test", default=1, help="Testing")
 @click.pass_context
 def test(ctx, test):
-	print(ctx.obj)
-	subprocess.call(["ls", "-lrt"])
+	click.secho("Commands are working are fine!, You're ready to go", fg="white")
+	
+
+
+@click.command("compare-files")
+@click.option("--file1", default=None, help="File1 Name with complete path")
+@click.option("--file2", default=None, help="File2 Name with complete path")
+@click.pass_context
+def compare_files(ctx, file1, file2):
+	if not file1  and file2:
+		click.secho("Required two files.", fg="yellow")
+		return
+	from datasci.operations.base import compare_files as _compare
+	_compare(file1, file2, {})
 
 
 
-
-commands = [test]
+commands = [test, compare_files]
